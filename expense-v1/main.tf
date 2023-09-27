834a01f2ab2a5daad230b30b096d7f6a8d834154
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-09-27 17:30:54 +0530

    expense scripts

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "frontend" {
 }
 resource "aws_route53_record" "frontend" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "frontend.${var.zone_id}"
+  name    = "1.${var.zone_id}"
   type    = "A"
   ttl     = 30
   records = [ aws_instance.frontend.private_ip ]
@@ -21,7 +21,7 @@ resource "null_resource" "frontend" {
 cd /home/centos/infra-ansible
 git pull
 sleep 60
-ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+ansible-playbook -i ${aws_instance.1.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
 EOF
   }
 }
@@ -38,7 +38,7 @@ resource "aws_instance" "backend" {
 
 resource "aws_route53_record" "backend" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "backend.${var.zone_id}"
+  name    = "3.${var.zone_id}"
   type    = "A"
   ttl     = 30
   records = [ aws_instance.backend.private_ip ]
@@ -51,7 +51,7 @@ resource "null_resource" "backend" {
 cd /home/centos/infra-ansible
 git pull
 sleep 60
-ansible-playbook -i ${aws_instance.backend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
+ansible-playbook -i ${aws_instance.3.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
 EOF
   }
 }
@@ -66,7 +66,7 @@ resource "aws_instance" "mysql" {
 }
 resource "aws_route53_record" "mysql" {
   zone_id = data.aws_route53_zone.zone.zone_id
-  name    = "mysql.${var.zone_id}"
+  name    = "2.${var.zone_id}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.mysql.private_ip]
@@ -79,7 +79,7 @@ resource "null_resource" "mysql" {
 cd /home/centos/infra-ansible
 git pull
 sleep 60
-ansible-playbook -i ${aws_instance.mysql.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
+ansible-playbook -i ${aws_instance.2.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
 EOF
   }
 }
