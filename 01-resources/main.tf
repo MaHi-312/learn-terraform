1442e1e420b5d8ecf26b0010cc6753bd7c89cd90
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-10-19 11:27:19 +0530

    expense scripts

--- 01-resources/main.tf
+++ 01-resources/main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "v5.21.0"
-    }
-  }
-}
-
-
 resource "aws_instance" "web" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
